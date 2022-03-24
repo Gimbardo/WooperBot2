a7b0577cd5f8f96f4d402f7dfbf6e1e98ded007a
Gimbardo <gamberoatomico@gmail.com>
2022-03-24 10:17:18 +0100

    #5 :rocket: scripts to deploy commands

--- deployers/global-deploy-commands.js
+++ deployers/global-deploy-commands.js
@@ -5,7 +5,6 @@ const { Routes } = require('discord-api-types/v9');
 require ('custom-env').env()
 
 const clientId = process.env.CLIENT_ID;
-const guildId = process.env.GUILD_ID;
 const token = process.env.TOKEN;
 
 const { SlashCommandBuilder } = require('@discordjs/builders');
@@ -14,13 +13,13 @@ const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`../commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
 	
---
+++ deployers/guild-remove-commands.js
@@ -0,0 +1,19 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+require ('custom-env').env()
+
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+const token = process.env.TOKEN;
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const rest = new REST({ version: '9' }).setToken(token);
+rest.get(Routes.applicationGuildCommands(clientId, guildId))
+    .then(data => {
+      const promises = [];
+      for (const command of data) {
+        const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`
+        promises.push(rest.delete(deleteUrl))
+      }
+      return Promise.all(promises)
+    })
