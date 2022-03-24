2f69875300c639f673c8c0d98cb5139487f378c3
Gimbardo <gamberoatomico@gmail.com>
2022-03-24 14:18:00 +0100

    #3 :sparkles: added roll and minor fix on flip

--- commands/flip.js
+++ commands/flip.js
@@ -3,8 +3,8 @@ const lib = require('../lib/utility')
 
 function flip(){
   if( lib.getRandomInt(0,1) )
-    return "Head"
-  return "Tail"
+    return "Head ⭕"
+  return "Tail ❌"
 }
 
 module.exports = {
---
+++ commands/roll.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const lib = require('../lib/utility')
+
+function roll(dice_type=20){
+  if( dice_type < 1 )
+    return `Valore Massimo ${dice_type} non valido :game_die:`
+
+  return `You rolled ${lib.getRandomInt(1,dice_type)} out of ${dice_type} :game_die:`
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('roll')
+		.setDescription('Rolls a d20, or any dice you want!')
+    .addIntegerOption(option => option.setName('dice').setDescription('Max value that can be rolled')),
+	async execute(interaction) {
+    let dice_type = interaction.options.getInteger('dice')
+    await interaction.reply(roll(dice_type));
+	},
+};
