1f14ff5d294d528824e57d04bf828f51c7bd7674
Gimbardo <gamberoatomico@gmail.com>
2022-03-26 12:16:32 +0100

    #3 :bug: fix for roll without args

--- commands/roll.js
+++ commands/roll.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const lib = require('../lib/utility')
 
-function roll(dice_type=20){
+function roll(dice_type){
   if( dice_type < 1 )
     return `Valore Massimo ${dice_type} non valido :game_die:`
 
@@ -14,7 +14,7 @@ module.exports = {
 		.setDescription('Rolls a d20, or any dice you want!')
     .addIntegerOption(option => option.setName('dice').setDescription('Max value that can be rolled')),
 	async execute(interaction) {
-    let dice_type = interaction.options.getInteger('dice')
+    let dice_type = interaction.options.getInteger('dice') || 20
     await interaction.reply(roll(dice_type));
 	},
 };
